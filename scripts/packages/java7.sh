5139e751688c1a0a4feb298c10e5a2c0cfd907fb
Darren Weber <dweber.consulting@gmail.com>
2017-03-29 14:48:06 -0700

    Remove java 7 package

--- scripts/packages/java7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script depends on the ./java.sh for installation of Oracle java dependencies
-
-echo "java 7 installation"
-apt-get install -y oracle-java7-installer
-yes "" | apt-get -f install
